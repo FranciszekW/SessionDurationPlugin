309b3f9f036cd64111e105b63f4b3df33520ae80
FranciszekW <franciow10@gmail.com>
2024-10-28 15:10:20 +0100

    A storage to store the session time, even when the widget is hidden in the IDE.

---
+++ src/main/java/franciszekw/sessiondurationplugin/SessionDurationState.java
@@ -0,0 +1,20 @@
+package franciszekw.sessiondurationplugin;
+
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+
+@State(name = "SessionDurationState", storages = @Storage("sessionDuration.xml"))
+public class SessionDurationState implements PersistentStateComponent<SessionDurationState> {
+    public long startTime; // Store as epoch milliseconds
+
+    @Override
+    public SessionDurationState getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(SessionDurationState state) {
+        this.startTime = state.startTime;
+    }
+}
