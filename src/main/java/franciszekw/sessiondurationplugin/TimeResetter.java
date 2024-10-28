5cb6cfe98dc6b9aa6b59f9d73531d4fd133934d3
FranciszekW <franciow10@gmail.com>
2024-10-28 17:26:36 +0100

    Class to reset the startupTime held in SessionDurationState.

---
+++ src/main/java/franciszekw/sessiondurationplugin/TimeResetter.java
@@ -0,0 +1,20 @@
+package franciszekw.sessiondurationplugin;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.ProjectActivity;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class TimeResetter implements ProjectActivity {
+    @Override
+    public @Nullable Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
+        SessionDurationState state = project.getService(SessionDurationState.class);
+        // Reset the timer to 0, so that it will be launched from the beginning next time
+        state.reset();
+
+        return null;
+    }
+}
