8f7d879cd22ef1d8aaea12ff309a69364f040600
FranciszekW <franciow10@gmail.com>
2024-10-28 12:44:39 +0100

    Added logging.

--- src/main/java/franciszekw/sessiondurationplugin/SessionDurationWidget.java
+++ src/main/java/franciszekw/sessiondurationplugin/SessionDurationWidget.java
@@ -1,5 +1,6 @@
 package franciszekw.sessiondurationplugin;
 
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.wm.StatusBarWidget;
 import com.intellij.openapi.wm.impl.status.TextPanel;
 import org.jetbrains.annotations.NotNull;
@@ -12,11 +13,14 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class SessionDurationWidget implements StatusBarWidget, StatusBarWidget.TextPresentation {
+    private final static Logger LOG = Logger.getInstance(SessionDurationWidget.class);
+
     private final TextPanel textPanel = new TextPanel();
     private final Timer timer;
     private final Instant startTime;
 
     public SessionDurationWidget() {
+        LOG.info("SessionDurationWidget created");
         this.startTime = Instant.now();
         // refresh the text every second
         this.timer = new Timer(1000, new ActionListener() {
@@ -36,27 +40,33 @@ public class SessionDurationWidget implements StatusBarWidget, StatusBarWidget.T
         long hours = minutes / 60;
         seconds = seconds % 60;
         minutes = minutes % 60;
-        textPanel.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+        String text = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        LOG.info("Updating text to " + text);
+        textPanel.setText(text);
     }
 
     @Override
     public @NotNull String ID() {
+        LOG.info("ID called");
         return "SessionDurationWidget";
     }
 
     @Override
     public @NotNull String getText() {
+        LOG.info("getText called");
         return textPanel.getText();
     }
 
     @Override
     public float getAlignment() {
+        LOG.info("getAlignment called");
         // allign to the right
         return 1.0f;
     }
 
     @Override
     public @Nullable String getTooltipText() {
+        LOG.info("getTooltipText called");
         return "Current session duration";
     }
 }
--- src/main/java/franciszekw/sessiondurationplugin/SessionDurationWidgetFactory.java
+++ src/main/java/franciszekw/sessiondurationplugin/SessionDurationWidgetFactory.java
@@ -1,5 +1,6 @@
 package franciszekw.sessiondurationplugin;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.StatusBarWidget;
@@ -8,25 +9,32 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class SessionDurationWidgetFactory implements StatusBarWidgetFactory {
+    private static final Logger LOG = Logger.getInstance(SessionDurationWidgetFactory.class);
+
     @Override
     public @NotNull @NonNls String getId() {
+        LOG.info("getId called");
         return "SessionDurationWidgetFactory";
     }
 
     @Override
     public @NotNull @NlsContexts.Label String getDisplayName() {
+        LOG.info("getDisplayName called");
         return "Session Duration";
     }
 
     public boolean isAvailable(@NotNull StatusBar statusBar) {
+        LOG.info("isAvailable called");
         return true;
     }
 
     public @NotNull StatusBarWidget createWidget(@NotNull StatusBar statusBar) {
+        LOG.info("createWidget called");
         return new SessionDurationWidget();
     }
 
     public void disposeWidget(@NotNull StatusBarWidget widget) {
+        LOG.info("disposeWidget called");
         // do nothing
     }
 }
