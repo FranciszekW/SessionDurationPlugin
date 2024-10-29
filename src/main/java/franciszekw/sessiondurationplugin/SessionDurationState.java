f2ac25c3126735c01d08e67b174ac1f27136bc1a
FranciszekW <franciow10@gmail.com>
2024-10-29 18:10:09 +0100

    Here lied the problem! PersistentState keeps the state unchanged across multiple IDE launches. All it took was to read the documentation...

--- src/main/java/franciszekw/sessiondurationplugin/SessionDurationState.java
+++ src/main/java/franciszekw/sessiondurationplugin/SessionDurationState.java
@@ -1,34 +1,16 @@
 package franciszekw.sessiondurationplugin;
 
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.diagnostic.Logger;
 
 /**
- * This class represents the state of the session duration plugin.
- * It implements the PersistentStateComponent interface to allow
- * the state to be persisted across hiding and showing the widget.
+ * This class represents the state of the session duration.
  */
-@State(name = "SessionDurationState", storages = @Storage("sessionDuration.xml"))
-public class SessionDurationState implements PersistentStateComponent<SessionDurationState> {
-    private final static Logger LOG = Logger.getInstance(SessionDurationWidget.class);
-
+@Service
+public final class SessionDurationState {
     // The start time of the session, stored as epoch milliseconds
     public long startTime;
-
-    @Override
-    public SessionDurationState getState() {
-        return this;
-    }
-
-    @Override
-    public void loadState(SessionDurationState state) {
-        this.startTime = state.startTime;
-    }
-
-    public void reset() {
-        LOG.info("SessionDurationState reset called");
-        this.startTime = 0;
-    }
 }
